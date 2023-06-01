07172931a4cbf9b350a2422a25395eb491f12c61
Arne Kiesewetter <banane9@live.de>
2023-06-01 16:21:34 +0200

    Integrate CustomUILib and change output to field

--- DynVarSpaceTree/DynVarSpaceTree.cs
+++ DynVarSpaceTree/DynVarSpaceTree.cs
@@ -5,11 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using BaseX;
-using CodeX;
 using FrooxEngine;
-using FrooxEngine.LogiX;
-using FrooxEngine.LogiX.Data;
-using FrooxEngine.LogiX.ProgramFlow;
 using FrooxEngine.UIX;
 using HarmonyLib;
 using NeosModLoader;
@@ -19,17 +15,17 @@ namespace DynVarSpaceTree
     public class DynVarSpaceTree : NeosMod
     {
         [AutoRegisterConfigKey]
-        private static readonly ModConfigurationKey<bool> EnableLinkedVariablesList = new ModConfigurationKey<bool>("EnableLinkedVariablesList", "Allow generating a list of dynamic variable definitions for a space.", () => true);
+        private static readonly ModConfigurationKey<bool> EnableLinkedVariablesList = new("EnableLinkedVariablesList", "Allow generating a list of dynamic variable definitions for a space.", () => true);
 
         [AutoRegisterConfigKey]
-        private static readonly ModConfigurationKey<bool> EnableVariableHierarchy = new ModConfigurationKey<bool>("EnableVariableHierarchy", "Allow generating a hierarchy of dynamic variable components for a space.", () => true);
+        private static readonly ModConfigurationKey<bool> EnableVariableHierarchy = new("EnableVariableHierarchy", "Allow generating a hierarchy of dynamic variable components for a space.", () => true);
 
         private static ModConfiguration Config;
 
         public override string Author => "Banane9";
         public override string Link => "https://github.com/Banane9/NeosDynVarSpaceTree";
         public override string Name => "DynVarSpaceTree";
-        public override string Version => "1.0.1";
+        public override string Version => "2.0.0";
 
         public override void OnEngineInit()
         {
@@ -37,56 +33,64 @@ namespace DynVarSpaceTree
             Config = GetConfiguration();
             Config.Save(true);
             harmony.PatchAll();
-            CustomUILib.CustomUILib.AddCustomInspector<DynamicVariableSpace>(WorkerInspectorPatches.BuildInspectorUI);
+            CustomUILib.CustomUILib.AddCustomInspector<DynamicVariableSpace>(BuildInspectorUI);
         }
 
-        private static class WorkerInspectorPatches
+        private static void BuildInspectorUI(DynamicVariableSpace space, UIBuilder ui)
         {
-            internal static void BuildInspectorUI(DynamicVariableSpace space, UIBuilder ui)
+            CustomUILib.CustomUILib.BuildInspectorUI(space, ui);
+
+            var outputField = ui.Current.AttachComponent<ValueField<string>>();
+
+            if (Config.GetValue(EnableLinkedVariablesList))
+                MakeButton(ui, "Output names of linked Variables", () => OutputVariableNames(space, outputField.Value));
+
+            if (Config.GetValue(EnableVariableHierarchy))
+                MakeButton(ui, "Output tree of linked Variable Hierarchy", () => OutputVariableHierarchy(space, outputField.Value));
+
+            SyncMemberEditorBuilder.BuildField(outputField.Value, "Output", outputField.GetSyncMemberFieldInfo("Value"), ui);
+        }
+
+        private static void MakeButton(UIBuilder ui, string text, Action action)
+        {
+            var button = ui.Button(text);
+            button.RequireLockInToPress.Value = true;
+
+            var valueField = button.Slot.AttachComponent<ValueField<bool>>().Value;
+
+            var toggle = button.Slot.AttachComponent<ButtonToggle>();
+            toggle.TargetValue.Target = valueField;
+
+            valueField.OnValueChange += field => action();
+        }
+
+        private static void OutputVariableHierarchy(DynamicVariableSpace space, Sync<string> target)
+        {
+            var hierarchy = new SpaceTree(space);
+
+            if (hierarchy.Process())
+                target.Value = hierarchy.ToString();
+            else
+                target.Value = "";
+        }
+
+        private static void OutputVariableNames(DynamicVariableSpace space, Sync<string> target)
+        {
+            var names = new StringBuilder("Variables linked to Namespace ");
+            names.Append(space.SpaceName);
+            names.AppendLine(":");
+
+            foreach (var identity in space._dynamicValues.Keys)
             {
-                CustomUILib.CustomUILib.BuildInspectorUI(space, ui);
-                if (Config.GetValue(EnableLinkedVariablesList))
-                {
-                    var namesButton = ui.Button("Copy Names of linked Variables", color.Pink);
-                    namesButton.LocalPressed += (button, data) => copyVariableNames(space);
-                    namesButton.RequireLockInToPress.Value = true;
-                }
-                if (Config.GetValue(EnableVariableHierarchy))
-                {
-                    var treeButton = ui.Button("Copy Tree of linked Variable Hierarchy", color.Pink);
-                    treeButton.LocalPressed += (button, data) => copyVariableHierarchy(space);
-                    treeButton.RequireLockInToPress.Value = true;
-                }
+                names.Append(identity.name);
+                names.Append(" (");
+                names.AppendTypeName(identity.type);
+                names.AppendLine(")");
             }
 
-            private static void copyVariableHierarchy(DynamicVariableSpace space)
-            {
-                var hierarchy = new SpaceTree(space);
+            names.Remove(names.Length - Environment.NewLine.Length, Environment.NewLine.Length);
 
-                if (hierarchy.Process())
-                    space.InputInterface.Clipboard.SetText(hierarchy.ToString());
-                else
-                    space.InputInterface.Clipboard.SetText("");
-            }
-
-            private static void copyVariableNames(DynamicVariableSpace space)
-            {
-                var names = new StringBuilder("Variables linked to Namespace ");
-                names.Append(space.SpaceName);
-                names.AppendLine(":");
-
-                foreach (var identity in space._dynamicValues.Keys)
-                {
-                    names.Append(identity.name);
-                    names.Append(" (");
-                    names.AppendTypeName(identity.type);
-                    names.AppendLine(")");
-                }
-
-                names.Remove(names.Length - Environment.NewLine.Length, Environment.NewLine.Length);
-
-                space.InputInterface.Clipboard.SetText(names.ToString());
-            }
+            target.Value = names.ToString();
         }
     }
 }
--- README.md
+++ README.md
@@ -3,10 +3,13 @@ Dynamic Variable Space Tree
 
 A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that adds buttons to the DynamicVariableSpace component in the inspector that allow copying all linked variable definitions or the whole hierarchy of linked dynamic variable components. 
 
+Depends on [CustomUILib](https://github.com/art0007i/CustomUILib) to add to the UI of the `DynamicVariableSpace` component.
+
 ## Installation
 1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
 2. Place [DynVarSpaceTree.dll](https://github.com/Banane9/NeosDynVarSpaceTree/releases/latest/download/DynVarSpaceTree.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
-3. Start the game. If you want to verify that the mod is working you can check your Neos logs.
+3. Place [CustomUILib.dll](https://github.com/art0007i/CustomUILib/releases/latest/download/CustomUILib.dll) into your `nml_mods` folder.
+4. Start the game. If you want to verify that the mod is working you can check your Neos logs.
 
 ## Trimmed Sample Outputs
 
